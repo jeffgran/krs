1983e4a7e453c1dcd659abab065c31f2ff1c9f61
Jeff Gran <jeff.gran@gmail.com>
2019-01-18 09:24:46 -0700

    add some more functions

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -1,3 +1,6 @@
+extern crate rand;
+use rand::prelude::*;
+
 pub fn gcd(a: i64, b: i64) -> i64 {
     if a == 0 {return b};
     if b == 0 {return a};
@@ -71,6 +74,59 @@ pub fn is_divisible(num: i64, divisor: i64) -> bool {
     return residue == 0;
 }
 
+pub fn is_prime_naive_trial_division(num: i64)  -> bool {
+    for i in 2..num {
+        if num % i == 0 {return false};
+    }
+    return true;
+}
+
+
+pub fn is_probable_prime_fermat(num: i64) -> bool {
+    let mut rng = thread_rng();
+    for _ in 1..15 {
+        let a: i64 = rng.gen_range(2, num - 1);
+        //println!("{} ^ {} % {} = {}", a, num - 1, num, mpow(a, num - 1, num));
+        if (mpow(a, num - 1, num)) != 1 { return false; }
+    }
+    return true
+}
+
+const FIRST_15_PRIMES: [i64; 15] = [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47];
+pub fn is_probable_prime_miller_rabin(num: i64) -> bool {
+    let mut m = num - 1;
+    let mut trail = Vec::new();
+    while m % 2 == 0 {
+        trail.insert(0, m);
+        m = m / 2;
+    }
+    println!("{:?}", trail);
+    for a in FIRST_15_PRIMES.iter() {
+        let x = mpow(*a, trail[0], num);
+        println!("{} ^ {} % {} = {}", *a, trail[0], num, mpow(*a, trail[0], num));
+        if x == 1 || x == -1 { continue; }
+
+        let mut found = false;
+        for d in trail.iter() {
+            // let y = mpow(x, 2, num);
+            // println!("{} ^ {} % {} = {}", x, 2, num, mpow(x, 2, num));
+            // if y == -1 { found = true; break; }
+            if mpow(*a, *d, num) == -1 { found = true; break; }
+        }
+        if found == true { continue; } else { return false; }
+    }
+
+    return true
+}
+
+pub fn rsa_encrypt(msg: i64, n: i64, e: u32) -> i64 {
+    return msg.pow(e) % n;
+}
+
+pub fn rsa_decrypt(ciphertext: i64, d: u32, n: i64) -> i64 {
+    return ciphertext.pow(d) % n;
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -128,6 +184,38 @@ mod tests {
         assert_eq!(mult_inverse(5, 7), Some(3));
         assert_eq!(mult_inverse(1, 7), Some(1));
         assert_eq!(mult_inverse(37, 12345), Some(6673));
+    }
+
+    #[test]
+    fn test_rsa_encrypt() {
+        assert_eq!(rsa_encrypt(9, 143, 7), 48);
+    }
+
+    #[test]
+    fn is_prime_naive_trial_division_test() {
+        assert_eq!(is_prime_naive_trial_division(3), true);
+        assert_eq!(is_prime_naive_trial_division(31), true);
+        assert_eq!(is_prime_naive_trial_division(143), false);
+        assert_eq!(is_prime_naive_trial_division(561), false);
+        assert_eq!(is_prime_naive_trial_division(1009), true);
+    }
+
+    #[test]
+    fn fermat_test() {
+        assert_eq!(is_probable_prime_fermat(191), true);
+        assert_eq!(is_probable_prime_fermat(1009), true);
+        assert_eq!(is_probable_prime_fermat(1000), false);
+        assert_eq!(is_probable_prime_fermat(561), false); // carmichael number! can be false positive.
+        assert_eq!(is_probable_prime_fermat(19), true);
+    }
+
+    #[test]
+    fn miller_rabin_test() {
+        assert_eq!(is_probable_prime_miller_rabin(191), true);
+        assert_eq!(is_probable_prime_miller_rabin(1009), true);
+        assert_eq!(is_probable_prime_miller_rabin(1000), false);
+        assert_eq!(is_probable_prime_miller_rabin(561), false); // carmichael number! can be false positive.
+        assert_eq!(is_probable_prime_miller_rabin(19), true);
     }
 
 }
--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ mod commands;
 #[derive(StructOpt)]
 #[structopt(name = "krs", about = "crypto.rs -- A Cryptography/Math CLI Library")]
 enum Cryptors {
-    #[structopt(name = "gcd", help = "Returns the greatest common divisor of two numbers <a> and <b>")]
+    #[structopt(name = "gcd", about = "Returns the greatest common divisor of two numbers <a> and <b>", help = "Usage: krs gcd <a> <b>")]
     Gcd {
         #[structopt(help = "First number")]
         a: i64,
@@ -13,7 +13,7 @@ enum Cryptors {
         b: i64
     },
 
-    #[structopt(name = "mpow", help = "Modular Exponentiation, i.e. a^b(mod n)")]
+    #[structopt(name = "mpow", about = "Modular Exponentiation, i.e. a^b(mod n)", help = "Usage: krs mpow <a> <b> <n>")]
     Mpow {
         #[structopt(help = "Base")]
         a: i64,
@@ -23,12 +23,42 @@ enum Cryptors {
         n: i64
     },
 
-    #[structopt(name = "isdiv", help = "Is <a> evenly divisible by <b>?")]
+    #[structopt(name = "isdiv", about = "Is <a> evenly divisible by <b>?", help = "Usage: krs isdiv  <a> <b>")]
     Isdiv {
         #[structopt(help = "Number")]
         a: i64,
         #[structopt(help = "Divisor")]
         b: i64,
+    },
+
+    #[structopt(name = "mi", about = "What is the multiplicative inverse of <a> mod <n> (if it exists)?", help = "Usage: krs mi <a> <n>")]
+    MultInverse {
+        #[structopt(help = "Number")]
+        a: i64,
+        #[structopt(help = "Modulus")]
+        n: i64,
+    },
+
+    #[structopt(name = "rsae", about = "RSA encrypt integer 'message'", help = "usage: krs rsae <message> <modulus> <exponent>")]
+    RsaEncrypt {
+        #[structopt(help = "Message")]
+        message: i64,
+        #[structopt(help = "Modulus")]
+        n: i64,
+        #[structopt(help = "Exponent")]
+        e: u32,
+    },
+
+    #[structopt(name = "isprime", about = "Is the number prime?", help = "usage: krs isprime <number>")]
+    IsPrime {
+        #[structopt(help = "Number")]
+        num: i64
+    },
+
+    #[structopt(name = "isfmt", about = "Is the number probably prime (per Fermat)?", help = "usage: krs isfmt <number>")]
+    IsFermat {
+        #[structopt(help = "Number")]
+        num: i64
     },
 
 }
@@ -40,5 +70,9 @@ main!(|args: Cryptors| {
         Cryptors::Gcd { a, b } => println!("{}", commands::gcd(a, b)),
         Cryptors::Mpow { a, b, n } => println!("{}", commands::mpow(a, b, n)),
         Cryptors::Isdiv { a, b } => println!("{}", commands::is_divisible(a, b)),
+        Cryptors::MultInverse { a, n } => println!("{:?}", commands::mult_inverse(a, n)),
+        Cryptors::RsaEncrypt { message, n, e } => println!("{:?}", commands::rsa_encrypt(message, n, e)),
+        Cryptors::IsPrime { num } => println!("{:?}", commands::is_prime_naive_trial_division(num)),
+        Cryptors::IsFermat { num } => println!("{:?}", commands::is_probable_prime_fermat(num)),
     }
 });
